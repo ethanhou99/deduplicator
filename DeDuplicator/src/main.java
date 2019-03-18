3a57a0d6a4d41edee60b181609362ef1bd9dc660
Zifan Wang <chesterw@bu.edu>
2019-03-18 16:23:51 -0400

    match same file using hash code(md5)

---
+++ DeDuplicator/src/main.java
@@ -0,0 +1,19 @@
+import java.io.FileNotFoundException;
+
+public class main {
+    public static void main(String[] args){
+        try{
+            String filePath = "/Users/zifanwang/Downloads/Web_paraJacobi.ppt";
+            String filePath2 = "/Users/zifanwang/Downloads/Web_paraJacobi1.ppt";
+            String hash32 = md5.md5HashCode32(filePath);
+            String hash321 = md5.md5HashCode32(filePath2);
+            if (hash32.compareTo(hash321)==0){
+                System.out.println("Same File!");
+            }
+            System.out.println(hash32);
+            System.out.println(hash321);
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+}
