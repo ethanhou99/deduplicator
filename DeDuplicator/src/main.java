4eda3f41544bd30a9025b95c6980b8065754aca3
Zifan Wang <chesterw@bu.edu>
2019-03-25 16:26:41 -0400

    test cases in main function

--- DeDuplicator/src/main.java
+++ DeDuplicator/src/main.java
@@ -5,13 +5,18 @@ import java.util.ArrayList;
 
 public class main {
     public static void main(String[] args){
+
+        String testFile = System.getProperty("user.dir")+"/sublinear-big-data1.pptx";
+        String testFile2 = System.getProperty("user.dir")+"/sublinear-big-data-1.pptx";
+
+        cmdLine info = new cmdLine();
+        split sp = new split();
+
         // test md5 case
         try{
-            String filePath = "/Users/zifanwang/Downloads/Web_paraJacobi.ppt";
-            String filePath2 = "/Users/zifanwang/Downloads/Web_paraJacobi1.ppt";
-            String hash32 = md5.md5HashCode32(filePath);
-            String hash321 = md5.md5HashCode32(filePath2);
-            if (hash32.compareTo(hash321)==0){
+            String hash32 = md5.md5HashCode32(testFile);
+            String hash32_1 = md5.md5HashCode32(testFile2);
+            if (hash32.compareTo(hash32_1)==0){
                 System.out.println("Same File!");
             }
         } catch (FileNotFoundException e){
@@ -19,7 +24,6 @@ public class main {
         }
 
         // test cmdLine
-        cmdLine info = new cmdLine();
         info.readIn(args);
         System.out.println(info.lockerPath);
         System.out.println(info.fileName);
@@ -28,9 +32,8 @@ public class main {
 
         // test split
         try{
-            String testFile = System.getProperty("user.dir")+"/sublinear-big-data-1.pptx";
-            ArrayList<String> files = split.splitFile(new File(testFile),1024,info.lockerPath);
-            System.out.println(files);
+            sp.splitFile(new File(testFile),1024,info.lockerPath);
+            System.out.println(sp.hashCodes);
         } catch(IOException e){
             e.printStackTrace();
         }
--- DeDuplicator/src/split.java
+++ DeDuplicator/src/split.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class split {
-    public static ArrayList splitFile(File f,int sizeOfFiles,String lockerPath) throws IOException {
+
+    public static ArrayList<String> hashCodes = new ArrayList<>();
+
+    public static void splitFile(File f,int sizeOfFiles,String lockerPath) throws IOException {
         int counter = 0;
 
         // bytes as basic unit 1MB = 2^10 kb = 2^20 b
@@ -13,7 +16,6 @@ public class split {
 
         byte[] buffer = new byte[sizeOfFiles];
         String FileName = f.getName();
-        ArrayList<String> files = new ArrayList<>();
 
         try (FileInputStream fis = new FileInputStream(f);
              BufferedInputStream bis = new BufferedInputStream(fis)) {
@@ -24,12 +26,11 @@ public class split {
                 //String filePartName = String.format("%s %03d", FileName, counter++);
                 String filePartName = md5.md5HashCode32(buffer);
                 File newFile = new File(lockerPath, filePartName);
-                files.add(filePartName);
+                hashCodes.add(filePartName);
                 try (FileOutputStream out = new FileOutputStream(newFile)) {
                     out.write(buffer, 0, bytesAmount);
                 }
             }
         }
-        return files;
     }
 }
